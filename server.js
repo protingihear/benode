75a5b9e06256ce923ba94649794c31401dff6fcc
fharlup <fajarmufid01@gmail.com>
2024-12-24 20:58:01 +0700

    cors update

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const routes = require('./routes');
-const PORT = 3000;
 
+const PORT = 3000;
+app.use(cors({ origin: '*' }));
 app.use(express.json());
 app.use('/', routes);
 
