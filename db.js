e932280ce84a8a9440db74b98289c847248e800a
fharlup <fajarmufid01@gmail.com>
2024-12-24 21:32:45 +0700

    FIX ENV

--- db.js
+++ db.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root', 
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER, 
     password: '', 
-    database: 'iheara', 
+    database: process.env.DB_NAME, 
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0,
